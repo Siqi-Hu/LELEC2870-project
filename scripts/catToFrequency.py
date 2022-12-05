b141e0eb5605b86f6435a721c71a5adb931da75a
Siqi-Hu <husiqi1018@gmail.com>
2022-12-05 17:21:12 +0100

    ...

---
+++ scripts/catToFrequency.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import torch
+from collections import Counter
+
+def catToFrequency(dataset, column_name, inplace=False):
+    column = dataset[column_name]
+    counts = Counter(column)
+
+    dict_cat_freq = {}
+
+    for category, freq in counts.most_common():
+        dict_cat_freq[category] = freq / len(column)
+
+    return dict_cat_freq
+
+X1 = pd.read_csv("X1.csv")
+dict_cat_freq = catToFrequency(X1['studio'], 'studio')
+torch.save(dict_cat_freq, "studio_freq")
+
